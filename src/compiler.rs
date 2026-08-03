6afb1913a0dda26c30ecae6e071292bfc649af8d
advgnd <advaith@gundu.me>
2026-08-03 10:03:06 -0400

    feat: add BFS orbit finder

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,12 +1,11 @@
-use std::collections::BTreeMap;
-
-use disjoint::DisjointSet;
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 
 use crate::core::{
-    CompiledMove, CompiledPuzzle, Move, Orbit, PieceState, PropertyMaxes, Puzzle, TransformIndex,
+    CompiledMove, CompiledPieceState, CompiledPuzzle, Move, Orbit, PieceState, PropertyMaxes,
+    Puzzle, TransformIndex,
 };
 
-fn compute_compound_totals(property_maxes: &PropertyMaxes) -> BTreeMap<String, i32> {
+fn compute_compiled_totals(property_maxes: &PropertyMaxes) -> BTreeMap<String, i32> {
     let mut totals = BTreeMap::new();
     let mut total = 1;
 
@@ -18,10 +17,13 @@ fn compute_compound_totals(property_maxes: &PropertyMaxes) -> BTreeMap<String, i
     totals
 }
 
-fn decode_compound_state(mut state: i32, property_maxes: &PropertyMaxes) -> PieceState {
+fn decode_compiled_state(
+    mut state: CompiledPieceState,
+    property_maxes: &PropertyMaxes,
+) -> PieceState {
     let mut piece_state = PieceState::new();
 
-    for (property, total) in compute_compound_totals(property_maxes).iter() {
+    for (property, total) in compute_compiled_totals(property_maxes).iter() {
         piece_state.insert(property.clone(), state / total);
         state %= total;
     }
@@ -29,30 +31,36 @@ fn decode_compound_state(mut state: i32, property_maxes: &PropertyMaxes) -> Piec
     piece_state
 }
 
-fn encode_compound_state(piece_state: &PieceState, property_maxes: &PropertyMaxes) -> i32 {
+fn encode_compiled_state(
+    piece_state: &PieceState,
+    property_maxes: &PropertyMaxes,
+) -> CompiledPieceState {
     let mut state = 0;
 
-    for (property, total) in compute_compound_totals(property_maxes).iter() {
+    for (property, total) in compute_compiled_totals(property_maxes).iter() {
         state += piece_state[property] * total;
     }
 
     state
 }
 
-fn compile_move(move_: Move, property_maxes: &PropertyMaxes, state: &[PieceState]) -> CompiledMove {
+fn compile_move(
+    move_: Move,
+    property_maxes: &PropertyMaxes,
+    states_map: &[String],
+) -> CompiledMove {
     let mut transform = vec![];
 
-    for piece_state in state.iter() {
+    for piece_properties in states_map {
         let mut row = vec![];
-        let piece_properties = piece_state.keys().collect::<Vec<_>>();
         let piece_property_maxes = property_maxes
             .iter()
-            .filter(|(property, _)| piece_properties.contains(property))
+            .filter(|(property, _)| piece_properties.contains(*property))
             .map(|(key, value)| (key.clone(), *value)) // Dereference because filter is referencing for whatever reason
             .collect::<BTreeMap<_, _>>();
 
         for piece_id in 0..piece_property_maxes.values().product() {
-            let piece_state = decode_compound_state(piece_id as i32, &piece_property_maxes);
+            let piece_state = decode_compiled_state(piece_id as i32, &piece_property_maxes);
             let piece_state = piece_state
                 .iter()
                 .map(|(property, value)| {
@@ -71,7 +79,7 @@ fn compile_move(move_: Move, property_maxes: &PropertyMaxes, state: &[PieceState
                 })
                 .collect::<PieceState>();
 
-            row.push(encode_compound_state(&piece_state, property_maxes));
+            row.push(encode_compiled_state(&piece_state, property_maxes));
         }
 
         transform.push(row);
@@ -80,5 +88,56 @@ fn compile_move(move_: Move, property_maxes: &PropertyMaxes, state: &[PieceState
     CompiledMove {
         name: move_.name,
         transform,
+    }
+}
+
+fn find_orbits(state_len: usize, moves: &[CompiledMove]) -> Vec<Orbit> {
+    let mut piece_state_map: HashMap<BTreeSet<i32>, Vec<i32>> = HashMap::new();
+
+    for piece_id in 0..state_len {
+        let mut new_states: BTreeSet<i32> = BTreeSet::from_iter(vec![0]);
+        let mut visited: BTreeSet<i32> = BTreeSet::from_iter(vec![]);
+
+        while !visited.is_superset(&new_states) {
+            visited.extend(new_states.iter());
+
+            for move_ in moves.iter() {
+                new_states = new_states
+                    .into_iter()
+                    .map(|state| move_.transform[piece_id][state as usize])
+                    .collect::<BTreeSet<i32>>();
+            }
+        }
+
+        piece_state_map
+            .entry(visited)
+            .or_default()
+            .push(piece_id as i32);
+    }
+
+    piece_state_map
+        .into_iter()
+        .map(|(visited, pieces)| Orbit {
+            pieces,
+            max_composite_state: visited.into_iter().max().unwrap_or(0),
+        })
+        .collect()
+}
+
+impl From<Puzzle> for CompiledPuzzle {
+    fn from(puzzle: Puzzle) -> Self {
+        let compiled_moves: Vec<CompiledMove> = puzzle
+            .moves
+            .into_iter()
+            .map(|move_| compile_move(move_, &puzzle.property_maxes, &puzzle.states_map))
+            .collect();
+        let orbits = find_orbits(puzzle.states_map.len(), &compiled_moves);
+
+        CompiledPuzzle {
+            moves: compiled_moves,
+            orbits,
+            property_maxes: puzzle.property_maxes,
+            states_map: puzzle.states_map,
+        }
     }
 }
--- src/core.rs
+++ src/core.rs
@@ -1,10 +1,11 @@
-use std::collections::{BTreeMap, HashSet};
+use std::collections::BTreeMap;
 
 use grid::Grid;
 use serde::{Deserialize, Serialize};
 
 pub type PropertyMaxes = BTreeMap<String, i32>; // i32 represents the max value of the property
 pub type PieceState = BTreeMap<String, i32>; // i32 represents the current value of the property
+pub type CompiledPieceState = i32; // compound state derived from the traditional piece state
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum TransformIndex {
@@ -27,14 +28,14 @@ pub struct Move {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Orbit {
     pub pieces: Vec<i32>,
-    pub properties: HashSet<String>,
+    pub max_composite_state: CompiledPieceState,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Puzzle {
     pub moves: Vec<Move>,
     pub property_maxes: PropertyMaxes,
-    pub state: Vec<PieceState>,
+    pub states_map: Vec<String>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -48,5 +49,5 @@ pub struct CompiledPuzzle {
     pub moves: Vec<CompiledMove>,
     pub orbits: Vec<Orbit>,
     pub property_maxes: PropertyMaxes,
-    pub state: Vec<PieceState>,
+    pub states_map: Vec<String>,
 }
